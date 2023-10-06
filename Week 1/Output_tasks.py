cdd51a2ffda9bc88170ede78be3fa61f09a61bee
Kayuus8664 <6akink41@solent.ac.uk>
2023-10-06 11:11:05 +0100

    Updated code to display a longer multi-line message

--- Week 1/Output_tasks.py
+++ Week 1/Output_tasks.py
@@ -1,3 +1,9 @@
 # Display message to the screen
 print("Welcome to COM411")
 print("in week 1 we will learn...")
+print()
+print("...how to use git and github")
+print("...how to output to the screen")
+print("...how to get user input")
+print()
+print("I hope you are enjoying the lesson thus far")
