549b03c0122f31d56e814f445d3511d640d87777
Kayode Akinwunmi <6akink41@solent.ac.uk>
2023-10-06 15:11:15 +0100

    Added code to demonstrate the use of an if...else statement

--- week 2/if_statements.py
+++ week 2/if_statements.py
@@ -3,3 +3,9 @@ book_type = input()
 if book_type == "adventure":
     print("I like adventure books!")
 print("Finished reading book.")
+activity = input("please enter activity")
+if activity == "calculate":
+    print("\nPerforming calculations...")
+else:
+    print("\nPerforming activity...")
+print("\nActivity completed!")
