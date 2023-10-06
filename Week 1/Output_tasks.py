2f53b830531fa1247cace542d82941235a933103
Kayuus8664 <6akink41@solent.ac.uk>
2023-10-06 11:17:19 +0100

    Added code to display escape characters

--- Week 1/Output_tasks.py
+++ Week 1/Output_tasks.py
@@ -7,3 +7,10 @@ print("...how to output to the screen")
 print("...how to get user input")
 print()
 print("I hope you are enjoying the lesson thus far")
+
+# Display escape characters
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a backslash")
+print("\"Displays a double quote")
+print("\' Displays a single quote")
