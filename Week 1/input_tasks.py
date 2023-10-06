6e0548aaf12df4195c85151004734d8bc0899d85
Kayode Akinwunmi <6akink41@solent.ac.uk>
2023-10-06 14:59:08 +0100

    Added code to demonstrate the use of string operators

--- Week 1/input_tasks.py
+++ Week 1/input_tasks.py
@@ -10,3 +10,10 @@ height = float(input("How tall are you (in meters)?"))
 weight = float(input("how much do you weigh (in kilograms)?"))
 BMI = weight/height**2
 print (f"{name}, you are {age} years old and your bmi is {BMI}")
+
+
+
+lives = int(input("Please enter the number of lives:"))
+energy = int(input("please enter the energy level"))
+shield = int(input("please enter the shield level"))
+print(f"\nHealth has been set\nLives:\t{'♥'*lives}\nEnergy:\t{'♦'*energy}\nShield:\t{'♦'*shield}")d
