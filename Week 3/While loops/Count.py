5497100965d1feea68a0366ab2e48eb064a9c302
Kayode Akinwunmi <6akink41@solent.ac.uk>
2023-11-10 15:15:09 +0000

    Added code to demonstrate the use of a while loop with a counter.

---
+++ Week 3/While loops/Count.py
@@ -0,0 +1,10 @@
+# Ask user for number of obstacles
+obstacles_to_avoid = int(input("How many obstacles should I avoid?"))
+obstacles_avoided = 0
+while obstacles_avoided < obstacles_to_avoid:
+    obstacles_avoided = obstacles_avoided + 1
+    print(f"Avoiding...Done! {obstacles_avoided} obstacles avoided.")
+
+
+
+
