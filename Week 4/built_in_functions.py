589294ebc8483085c6378c78af12389f22418438
Kayuus8664 <6akink41@solent.ac.uk>
2023-12-18 10:38:00 +0000

    Added code to demonstrate the use of built-in functions: len, ord.

---
+++ Week 4/built_in_functions.py
@@ -0,0 +1,10 @@
+print("Program Started!")
+standard_character = input("Please enter a standard character:")
+value = ord(standard_character)
+if len(standard_character) == 1:
+    print(value)
+else:
+    print("error!")
+
+print("Program Ended!")
+
