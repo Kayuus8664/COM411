258179273fb3d2cacb41f310148d20ae4f1a45e4
Kayuus8664 <6akink41@solent.ac.uk>
2023-12-18 12:10:00 +0000

    Added code to demonstrate the use of a user-defined function with a nested decision.

---
+++ Week 4/custom_functions/Nesting.py
@@ -0,0 +1,8 @@
+def identify():
+    word = input("enter a word representing what you see")
+    if word ==  "a large boulder":
+        print("It's time to run!")
+    else:
+        print("We will be fine.")
+
+identify()
